0c86a406ff5f44a064b0060e8618fcacb6e0c686
leobearded <42157701+leobearded@users.noreply.github.com>
2018-11-19 21:25:41 -0200

    Sensors-updated-1911

--- Sensors.py
+++ Sensors.py
@@ -3,56 +3,68 @@ import MySQLdb as db
 import time
 import sys
 
-#Startup and configuration pins
+#Startup configuration
 IO.setwarnings(False)
 IO.setmode(IO.BCM)
 
 #Input pins
-IO.setup(6,IO.IN)
-IO.setup(13,IO.IN)
-IO.setup(19,IO.IN)
-IO.setup(26,IO.IN)
+IO.setup(4, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(13, IO.IN)
+IO.setup(19, IO.IN)
+IO.setup(26, IO.IN)
 
 #Output pins
-IO.setup(12,IO.OUT)
-IO.setup(16,IO.OUT)
-IO.setup(20,IO.OUT)
-IO.setup(21,IO.OUT)
+IO.setup(12, IO.OUT)
+IO.setup(16, IO.OUT)
+IO.setup(20, IO.OUT)
+IO.setup(21, IO.OUT)
 
 print("Starting system...")
 
 #Database configuration/connection
 database = db.connect('localhost', 'monitor', 'password', 'packages')
 cur = database.cursor()
-
 time.sleep(10)
 
+#Activate outputs
+IO.output(12, 1)
+IO.output(16, 1)
+IO.output(20, 1)
+IO.output(21, 1)
 x = 0
+
 while 1:
-    #Checks if GPIO pins received any signal
-    #Shutdown occurs after 10 failed detection attempts
-    if(IO.input(6)==True and x == 10):
-        #set pin 12 high
+    #Checks if GPIO pins received any signal;
+    #shutdown occurs after 10 failed detection attempts
+    if(x == 10):
+        IO.output(12, 0)
+
+    if(IO.input(4) == False and x == 10):
+        IO.output(12, 1)
         time.sleep(5)
         continue
-    if(IO.input(13)==True):
+    
+    if(IO.input(16) == True):
         with database:
             cur.execute("INSERT INTO log VALUES(default, NOW(), 1)")
             print("Data committed!")
             time.sleep(5)
             continue
-    if(IO.input(19)==True):
+
+    if(IO.input(20) == True):
         with database:
             cur.execute("INSERT INTO log VALUES(default, NOW(), 2)")
             print("Data committed!")
             time.sleep(5)
             continue
-    if(IO.input(26)==True):
+
+    if(IO.input(21) == True):
         with database:
             cur.execute("INSERT INTO log VALUES(default, NOW(), 3)")
             print("Data committed!")
             time.sleep(5)
             continue
+
     else:
         print("Nothing detected on sensors!")
         x = x + 1
@@ -63,6 +75,7 @@ while 1:
 print("Closing database...")
 database.close()
 time.sleep(5)
+
 print("Shutting down system...")
 time.sleep(5)
 sys.exit()
